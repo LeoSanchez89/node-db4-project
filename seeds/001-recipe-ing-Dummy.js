d60e02287591155893ee2f7c8f71034e1dfbbf49
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-03-19 20:45:21 -0400

    seed data complete

--- seeds/001-recipe-ing-Dummy.js
+++ seeds/001-recipe-ing-Dummy.js
@@ -1,19 +1,19 @@
 exports.seed = async function (knex) {
   
 	const recIngData = [
-		{ recipe_id: 1, ingredients_id: 1, ingredients_quantity: 1 },
-		{ recipe_id: 1, ingredients_id: 2, ingredients_quantity: .5 },
-		{ recipe_id: 1, ingredients_id: 3, ingredients_quantity: 1.5 },
+		{ recipes_id: 1, ingredients_id: 1, ingredient_quantity: 1 },
+		{ recipes_id: 1, ingredients_id: 2, ingredient_quantity: .5 },
+		{ recipes_id: 1, ingredients_id: 3, ingredient_quantity: 1.5 },
   
-    { recipe_id: 2, ingredients_id: 4, ingredients_quantity: 2 },
-		{ recipe_id: 2, ingredients_id: 5, ingredients_quantity: 3 },
-		{ recipe_id: 2, ingredients_id: 6, ingredients_quantity: 3 },
+    { recipes_id: 2, ingredients_id: 4, ingredient_quantity: 2 },
+		{ recipes_id: 2, ingredients_id: 5, ingredient_quantity: 3 },
+		{ recipes_id: 2, ingredients_id: 6, ingredient_quantity: 3 },
     
-    { recipe_id: 3, ingredients_id: 7, ingredients_quantity: 1 },
-		{ recipe_id: 3, ingredients_id: 8, ingredients_quantity: 1 },
-		{ recipe_id: 3, ingredients_id: 9, ingredients_quantity: .5 },
-		{ recipe_id: 3, ingredients_id: 2, ingredients_quantity: 1 },
-		{ recipe_id: 3, ingredients_id: 3, ingredients_quantity: .5 }
+    { recipes_id: 3, ingredients_id: 7, ingredient_quantity: 1 },
+		{ recipes_id: 3, ingredients_id: 8, ingredient_quantity: 1 },
+		{ recipes_id: 3, ingredients_id: 9, ingredient_quantity: .5 },
+		{ recipes_id: 3, ingredients_id: 2, ingredient_quantity: 1 },
+		{ recipes_id: 3, ingredients_id: 3, ingredient_quantity: .5 }
 	];
 
 	await knex("recipe_ingredients").truncate();
